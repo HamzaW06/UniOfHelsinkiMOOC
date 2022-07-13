5ec782fb2a2d62f458246ad655cc4e470907c825
unknown <Hamzaw.w06@gmail.com>
2022-07-13 16:21:51 -0500

    completed part 5 exercise 9

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,16 @@
-
 public class HealthStation {
 
+    private int weighingCount = 0;
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        weighingCount ++;
+        return person.getWeight();
+    }
+    public void feed(Person person){
+        person.setWeight(person.getWeight() + 1);
+    }
+    public int weighings(){
+        return weighingCount;
     }
 
 }
