770592194c97fe2530cec13da93ed27fcb79f29f
unknown <Hamzaw.w06@gmail.com>
2022-07-14 16:10:57 -0500

    completed part 5 exercise 11

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,26 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
-
+    public boolean largerThan(Apartment compared){
+        if (this.squares > compared.squares){
+            return true;
+        }
+        return false;
+    }
+    public int priceDifference(Apartment compared){
+        int a1 = (this.squares * this.princePerSquare);
+        int a2 = (compared.squares * compared.princePerSquare);
+        if (a1 > a2){
+            return a1-a2;
+        }
+        return a2-a1;
+    }
+    public boolean moreExpensiveThan(Apartment compared) {
+        int a1 = (this.squares * this.princePerSquare);
+        int a2 = (compared.squares * compared.princePerSquare);
+        if (a1 > a2){
+            return true;
+        }
+        return false;
+    }
 }
